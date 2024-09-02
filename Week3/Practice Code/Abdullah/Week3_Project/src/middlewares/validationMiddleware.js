5728c7af6ceb44d54eb9c6f5c6f1a97073aec488
Abdullah Amir <abdullahamirr2004@gmail.com>
2024-09-02 19:25:36 +0500

    updated version

--- Week3/Practice Code/Abdullah/Week3_Project/src/middlewares/validationMiddleware.js
+++ Week3/Practice Code/Abdullah/Week3_Project/src/middlewares/validationMiddleware.js
@@ -14,11 +14,11 @@ export const validatorTask = (req, res, next) => {
         return res.status(400).json({ message: 'Description must be a string' });
     }
 
-    const validStatus = ['pending', 'in-progress', 'complete'];
-    if(validStatus===undefined){
+    const Status = ['pending', 'in-progress', 'complete'];
+    if(Status===undefined){
         res.status(400).json({ message: 'staus is required' });
     }
-    if (status && !validStatus.includes(status)) {
+    if (status && !Status.includes(status)) {
         return res.status(400).json({ message: 'Invalid status. Must be one of: pending, in-progress, complete' });
     }
 
@@ -34,7 +34,7 @@ export const validatorTask = (req, res, next) => {
 };
 
 
- export const  updateValidatuion = (req, res, next) => {
+ export const  updateValidation = (req, res, next) => {
     const { title, description, status, priority } = req.body;
     if (typeof title !== "string" && title  !== undefined) {
         return res.status(400).json({ message: "Title must be a string" });
@@ -42,11 +42,8 @@ export const validatorTask = (req, res, next) => {
     if (description !== undefined && typeof description !== 'string') {
         return res.status(400).json({ message: 'Description must be a string' });
     }
-    const validStatus = ['pending', 'in-progress', 'complete'];
-    if(validStatus===undefined){
-        res.status(400).json({ message: 'staus is required' });
-    }
-    if (status && !validStatus.includes(status)) {
+    const Status = ['pending', 'in-progress', 'complete'];
+    if (status && !Status.includes(status)) {
         return res.status(400).json({ message: 'Invalid status. Must be one of: pending, in-progress, complete' });
     }
     const validPriority = ['low', 'medium', 'high'];
@@ -54,6 +51,6 @@ export const validatorTask = (req, res, next) => {
         return res.status(400).json({ message: 'Invalid priority. Must be one of: low, medium, high' });
     }
 
-    next(); // If all checks pass, proceed to the next middleware or route handler
+    next(); 
 };
 
--- Week3/Practice Code/Abdullah/Week3_Project/src/routes/taskrouter.js
+++ Week3/Practice Code/Abdullah/Week3_Project/src/routes/taskrouter.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { getTasks, createTask, getTaskById, delTaskById, updateTask } from '../controllers/taskcontroller.js';
 import { validatorTask } from '../middlewares/validationMiddleware.js';
-import { updateValidatuion } from '../middlewares/validationMiddleware.js';
+import { updateValidation } from '../middlewares/validationMiddleware.js';
 const router = express.Router();
 
 router.get('/', getTasks);
 router.post('/', validatorTask, createTask);
 router.get('/:id', getTaskById);
 router.delete('/:id', delTaskById);
-router.put('/:id',updateValidatuion, updateTask);
+router.put('/:id',updateValidation, updateTask);
 
 export default router;
